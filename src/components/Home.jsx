6e574df250bdb59eaf89386455591564e9292b3e
NITIN KUMAR <nitinkumarraj15@gmail.com>
2024-05-03 14:31:49 +0530

    vanilla tilt dependency installed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <div className=' flex flex-wrap p-5 bg-black text-white justify-evenly'>
+
                 <motion.div
                     initial={{ x: -100, opacity: 0 }}
                     animate={{ x: 0, opacity: 1 }}
@@ -17,11 +18,11 @@ const Home = () => {
                         transition={{ ease: "easeInOut", duration: 3 }}
                         className="text-[23px] text-center font-serif font-bold my-3">What is COLCON AI?</motion.h1>
                     <img src={logo} alt="image" className=" h-[200px] w-[450px] rounded-2xl my-2 text-center" />
-                    <p
+                    <motion.p
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         transition={{ ease: "easeInOut", duration: 3 }}
-                        className="mt-4 font-medium">COLCON AI is an advanced artificial intelligence platform designed to revolutionize communication. It empowers users with cutting-edge natural language processing capabilities, streamlining interactions and enhancing productivity.</p>
+                        className="mt-4 font-medium">COLCON AI is an advanced artificial intelligence platform designed to revolutionize communication. It empowers users with cutting-edge natural language processing capabilities, streamlining interactions and enhancing productivity.</motion.p>
                 </motion.div>
 
                 <motion.div
